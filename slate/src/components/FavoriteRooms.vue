eb9a34d103dc559db5941189251c5c6cbc745cf3
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-10-07 12:35:35 -0700

    Scaffolding.

---
+++ slate/src/components/FavoriteRooms.vue
@@ -0,0 +1,26 @@
+<template>
+  <div >
+    FAVORITE ROOMS
+    <ul>
+        <li v-for="(room, index) in rooms" :key="index"> {{ room }} </li>     
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'FavoriteRooms',
+    props: {
+        rooms: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    
+</style>
