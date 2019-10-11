6238fcaeb1658fd7532aaa6840ae04747b3d21a6
Brandon Hurrington <brandonhurrington@yahoo.com>
2019-10-10 19:19:41 -0700

    Big style changes

--- slate/src/components/Home.vue
+++ slate/src/components/Home.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <img src='../assets/global_chat.jpg' id="global-chat-image"/>
+    <p id="app-blurb">
+      Our team hails from a diverse background of heritage and experiences, we represent Nepal, Korea, the US, Poland, Africa, and many other countries.
+      We wanted to develop an app that would bring people closer together and serve to make a big world just a little bit smaller and more knowable.
+    </p>
+  </div>
 </template>
 
 <script>
@@ -7,3 +13,15 @@ export default {
   name: 'Home',
 }
 </script>
+
+<style scoped>
+  #app-blurb {
+    margin: .5em;
+    padding: .5em;
+  }
+ 
+  #global-chat-image {
+    margin: .5em;
+    padding: .5em;
+  }
+</style>
